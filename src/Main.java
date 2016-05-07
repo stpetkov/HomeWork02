fc314b4c3f5f62f288c82ae7748249babaf4af2a
Stanimir <stpetkov4@gmail.com>
2016-05-07 18:39:25 +0300

    This is HomeWork02.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// 1 задача
+		// 1 task
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Въведи дължина (a) : ");
+		System.out.print("Lenght (a) : ");
 		float a=sc.nextFloat();
-		System.out.print("Въведи ширина (b) : ");
+		System.out.print("Latitude (b) : ");
 		float b=sc.nextFloat();
 		System.out.println("S = a*b = "+(a*b));
-		// 2 задача
+		// 2 task
 		int c=5;
 		int d=8;
 		System.out.println("c = "+c+" d ="+d);
@@ -18,46 +18,46 @@ public class Main {
 		c=d;
 		d=f;
 		System.out.println("c = "+c+" d ="+d);
-		// 3 задача
+		// 3 task
 		String firstName=sc.nextLine();		
 		String lastName=sc.nextLine();
 		char sex=sc.next().charAt(0);
 		short years=sc.nextShort();		
 		long egn=sc.nextLong();
 		long number=sc.nextLong();
-		System.out.println((firstName+" "+lastName)+" на "
-		+years+" години. | пол: "+sex+" | ЕГН : "+egn+
-		" | Телефонен номер: "+number);
-		// 4 задача
+		System.out.println((firstName+" "+lastName)+" is "
+		+years+" years old. | sex: "+sex+" | EGN : "+egn+
+		" | Phone number: "+number);
+		// 4 task
 		String book="Harry Potter";
 		System.out.print("I will read the '"+book+"' book collection.");
-		// 5 задача
+		// 5 task
 		int q=sc.nextInt();
 		int w=sc.nextInt();
 		int e=sc.nextInt();
 		int r=sc.nextInt();
-		System.out.println(" Сумата на числата е : "+(q+w+e+r));
-		// 6 задача
-		System.out.print("Въведи радиус : ");
+		System.out.println(" The sum of the numbers is : "+(q+w+e+r));
+		// 6 task
+		System.out.print("Enter radius : ");
 		float radius=sc.nextFloat();
 		System.out.println("P = "+(2*Math.PI*radius));
 		System.out.println("S = "+(Math.PI*radius*radius));
-		// 7 задача
+		// 7 task
 		boolean division=true;
 		int numberR=sc.nextInt();
 		if(numberR%5==0 && numberR%7==0) division=true;
 		else division=false;
 		System.out.println(division);
-		// 8 задача
+		// 8 task
 		int oddOReven=sc.nextInt();
 		if(oddOReven%2==0) System.out.println("Even");
 		else System.out.print("Odd");
-		// 9 задача
+		// 9 task
 		int y=sc.nextInt();
 		int u=sc.nextInt();
 		System.out.println(y%u);
 		System.out.println(u%y);
-		// задача 10
+		// 10 task
 		int side=sc.nextInt();
 		int height=sc.nextInt();
 		System.out.println("S = (side*height)/2 = "+(side*height)/2);
