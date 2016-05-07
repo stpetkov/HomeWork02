f35cc8ae7e403e21c51ba6ba0c812c914961f9d7
Stanimir <stpetkov4@gmail.com>
2016-05-07 17:19:26 +0300

    This is HomeWork02.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Въведи дължина (a) : ");
+		float a=sc.nextFloat();
+		System.out.print("Въведи ширина (b) : ");
+		float b=sc.nextFloat();
+		System.out.println("S = a*b = "+(a*b));
+
+	}
+
+}
